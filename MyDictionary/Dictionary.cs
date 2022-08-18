b26ed6cb2390f25ca9813920f7680cfaf86b6974
bugra <bugra@DESKTOP-F81L0OF>
2022-08-18 20:21:01 +0300

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary <B, E>
+    {
+        B[] _keyArray;
+        E[] _valueArray;
+
+        public MyDictionary()
+        {
+            _keyArray = new B[0];
+            _valueArray = new E[0];
+        }
+        public void Add(B key, E value)
+        {
+            B[] tempKeyArray;
+            E[] tempValueArray;
+
+            tempKeyArray = _keyArray;
+            tempValueArray = _valueArray;
+
+            _keyArray = new B[_keyArray.Length];
+            _valueArray = new E[_valueArray.Length];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = tempKeyArray[i];
+                _valueArray[i] = tempValueArray[i];
+            }
+
+            _keyArray[_keyArray.Length - 1] =key;
+            _valueArray[_valueArray.Length - 1] = value;
+        }
+      
+
+        public int Count
+        {
+            get { return _keyArray.Length; }
+           
+        }
+        public B[] Keys
+        {
+            get { return _keyArray; }
+
+        }
+        public E[] Values
+        {
+            get { return _valueArray; }
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> sehirler = new Dictionary<string, int>();
+            sehirler.Add("Samsun" , 55);
+            sehirler.Add("İstanbul", 34);
+            sehirler.Add("Edirne", 22);
+            sehirler.Add("Ankara", 06);
+            sehirler.Add("İzmir", 35);
+
+            Console.WriteLine(sehirler.Count);
+
+            foreach (var key in sehirler.Keys)
+            {
+                Console.WriteLine("Sehirler: " + key );
+            }
+            foreach (var value in sehirler.Values)
+            {
+                Console.WriteLine("Plaka: " + value);
+            }
+
+
+
+
+
+        }
+    }
+}
